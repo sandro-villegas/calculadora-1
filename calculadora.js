a4889334fc79adb710630b7eb6fcb1366010eaa5
sandro villegas <zony123rbd@gmail.com>
2022-07-11 09:09:55 -0500

    inicio del programa

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+let suma;
+let resta;
+let multiplicacion;
+let divi;
+const a=1;
+const b=2;
+
+suma=a+b;
+resta=a-b;
+multiplicacion=a*b;
+divi=a/b;
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(divi)
