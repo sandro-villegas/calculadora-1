7f54c39de286231c03e3aab3d90a5a0166cbb62c
Pavel Leonardo Mansilla Goyas <pavel.mansilla@gmail.com>
2022-07-11 15:53:53 -0500

    primera version

--- calculadora.js
+++ calculadora.js
@@ -1,15 +1,82 @@
-let suma;
-let resta;
-let multiplicacion;
-let divi;
-const a=1;
-const b=2;
+let operador="";
+let number1="";
+let number2="";
+let resultado=0;
 
-suma=a+b;
-resta=a-b;
-multiplicacion=a*b;
-divi=a/b;
-console.log(suma);
-console.log(resta);
-console.log(multiplicacion);
-console.log(divi)
+let input = document.getElementById("input")
+input.textContent=resultado
+let calc = document.getElementById("calc");
+calc.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("number")){
+        if(!operador){
+            number1 = number1 + e.target.textContent
+            input.textContent = number1
+        }
+        else{
+            number2 = number2 + e.target.textContent
+            input.textContent = number2
+        }
+    }
+    if(e.target.classList.contains("operator")){
+        if(!operador){
+            operador=e.target.id;
+        }
+    }
+    if(e.target.id==="result"){
+        
+        if(operador==="+"){
+            resultado = parseFloat(number1)+parseFloat(number2)
+            console.log("resultado",resultado)
+            input.textContent = resultado
+            number1=""
+            number2=""
+            resultado=0
+            operador=""
+        }
+        if(operador==="-"){
+            resultado = parseInt(number1)-parseInt(number2)
+            input.textContent = resultado
+            /*input.textContent = input.textContent + resultado*/
+            number1=""
+            number2=""
+            resultado=0
+            operador=""
+        }
+        if(operador==="*"){
+            resultado = parseInt(number1)*parseInt(number2)
+            input.textContent = resultado
+            /*input.textContent = input.textContent + resultado*/
+            number1=""
+            number2=""
+            resultado=0
+            operador=""
+        }
+        if(operador==="/"){
+            resultado = parseInt(number1)/parseInt(number2)
+            input.textContent = resultado
+            /*input.textContent = input.textContent + resultado*/
+            number1=""
+            number2=""
+            resultado=0
+            operador=""
+        }
+    }
+    if(e.target.id==="clear"){
+        console.log("estoy en clkear")
+            number1=""
+            number2=""
+            resultado=0
+            operador=""
+            input.textContent = 0
+    }
+    if(e.target.id==="point"){
+        if(!operador){
+            number1 = number1 + "."
+            input.textContent=number1;
+        }
+        else{
+            number2 = number2 + "."
+            input.textContent=number2
+        }
+    }
+})
